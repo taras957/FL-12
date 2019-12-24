92f6515046c9d8bbc71aaf3a50c666f0728eb6e9
Taras <kornyaktaras@gmail.com>
2019-12-24 23:06:17 +0200

    last commit

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,51 +1,28 @@
 // Your code goes here
-
-let a;
-let b;
-let c;
-let x;
-let x1;
-let x2;
 let discrimCoef = 4;
 let toPow = 2;
 let devidedBy2 = 2;
-while (!a) {
-    a = +prompt('input number a', 1);
-    if (isNaN(parseFloat(a)) && !isFinite(a) || a === 0) {
-        alert('Invalid input data');
-    } else {
-        break;
-    }
+let a = prompt('input number a');
+let b = prompt('input number b');
+let c = prompt('input number c');
+let validFlag = true;
+
+if (isNaN(a) && !isFinite(a) || isNaN(b) && !isFinite(b) || isNaN(c) && !isFinite(c)) {
+    alert('Invalid input data');
+    validFlag = false;
 }
+while (validFlag) {
+    let d = Math.pow(b, toPow) - discrimCoef * a * c;
+    console.log(d);
 
-while (!b) {
-    b = +prompt('input number b', 1);
-    if (isNaN(parseFloat(b)) && !isFinite(b)) {
-        alert('Invalid input data');
+    if (d < 0) {
+        alert('There is no solution')
+    } else if (d === 0) {
+        x = -b / devidedBy2 * a;
+        alert(`X = ${x}`);
     } else {
-        break;
+        let x1 = Math.round((-b + Math.sqrt(d)) / devidedBy2 * a);
+        let x2 = Math.round((-b - Math.sqrt(d)) / devidedBy2 * a);
+        alert(`x1 = ${x1} and x2 = ${x2}`)
     }
-}
-
-while (!c) {
-    c = +prompt('input number c', 1);
-    if (isNaN(parseFloat(c)) && !isFinite(c)) {
-        alert('Invalid input data');
-    } else {
-        break;
-    }
-}
-
-let d = Math.pow(b, toPow) - discrimCoef * a * c;
-console.log(d);
-
-if (d < 0) {
-    alert('There is no solution')
-} else if (d === 0) {
-    x = -b / devidedBy2 * a;
-    alert('X = ' + x);
-} else {
-    let x1 = Math.round((-b + Math.sqrt(d)) / devidedBy2 * a);
-    let x2 = Math.round((-b - Math.sqrt(d)) / devidedBy2 * a);
-    alert(`x1 = ${x1} and x2 = ${x2}`)
 }
