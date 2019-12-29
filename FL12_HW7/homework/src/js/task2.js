1e18594ec1e97e65d873e5cc9a14f7d0c66980b3
Taras <kornyaktaras@gmail.com>
2019-12-29 12:26:47 +0200

    second task is done

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -9,107 +9,121 @@ if (!gameStart) {
     let totlaMaxPrize = 100;
     let maxPrize = totlaMaxPrize;
     let random = Math.floor(Math.random() * initialRange);
-    console.log(random);
 
 
-    while (counter > 0) {
+    while (counter >= 0) {
+        console.log('random'+ random);
+        
         console.log("total money " + userPrize);
         console.log('maxTotalPrize' + totlaMaxPrize);
-
-
-        // counter === 2 ? maxPrize / 2 : counter === 1 ? maxPrize / 4 : maxPrize
-        --counter
-        let userBet = +prompt(`Choose roulette pocket number from 0 to ${initialRange - 1}\nAttempts left: ${counter+1}\nTotal prize: ${ userPrize} $\nPossible prize on current attempt: ${maxPrize} $\nEnter a number of pocket`);
-
-
-        if (userBet === random && counter === 2) {
-            console.log('counter ' + counter);
-            userPrize += maxPrize;
-            let continueGame = confirm(`Congratulation, you won!   Your prize is: 
-            ${userPrize} $. Do you want to continue?`);
-            if (!continueGame) {
-                alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
-                let playAgain = confirm('Do you want to play again?');
-                if (playAgain) {
-                    initialRange = 9;
-                    totlaMaxPrize = 100;
-                    userPrize = 0;
-                    counter = 3;
-                    continue;
-                } else {
-                    break;
-                }
-            } else if (continueGame) {
-                initialRange += 4;
-                totlaMaxPrize *= 2;
-                maxPrize = totlaMaxPrize;
-                counter = 3;
-                continue;
-            }
-        } else if (userBet === random && counter === 1) {
-            console.log('counter ' + counter);
-            userPrize += maxPrize;
-            continueGame = (`Congratulation, you won!   Your prize is: ${userPrize} $. Do you want to continue?`);
-            if (!continueGame) {
-                alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
-                playAgain = confirm('Do you want to play again?')
-                if (playAgain) {
-                    initialRange = 9;
-                    totlaMaxPrize = 100;
-                    userPrize = 0;
-                    counter = 3;
-                    continue;
-                } else {
-                    break;
-                }
-
-            } else if (continueGame) {
-                initialRange += 4;
-                totlaMaxPrize *= 2;
-                maxPrize = totlaMaxPrize
-                counter = 3;
-                continue;
-            }
-        } else if (userBet === random && counter === 0) {
-            console.log('counter ' + counter);
-            userPrize += maxPrize;
-            continueGame = (`Congratulation, you won!   Your prize is: ${userPrize} $. Do you want to continue?`);
-            if (!continueGame) {
-                alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
-                playAgain = confirm('Do you want to play again?')
-                if (playAgain) {
-                    initialRange = 9;
-                    totlaMaxPrize = 100;
-                    maxPrize = totlaMaxPrize;
-                    userPrize = 0;
-                    counter = 3;
-                    continue;
-                } else {
-                    break;
-                }
-
-            } else if (continueGame) {
-                initialRange += 4;
-                totlaMaxPrize *= 2;
-                maxPrize = totlaMaxPrize;
-                counter = 3;
-                continue;
-            }
-        } else if (!userBet || userBet !== random) {
-            maxPrize /= 2;
-            continue;
-        } else {
+        if (counter === 0) {
             alert(`Thank you for your participation. Your prize is: ${userPrize} $`);
             playAgain = confirm('Do you want to play again?');
-            if (!playAgain) {
-                break;
-            } else {
+            if (playAgain) {
                 initialRange = 9;
                 userPrize = 0;
                 totlaMaxPrize = 100;
                 counter = 3;
-                continue;
+            } else if (!playAgain) {
+                break
             }
-        }
+        } 
+
+            --counter
+            let userBet = +prompt(`Choose roulette pocket number from 0 to ${initialRange - 1}\nAttempts left: ${counter + 1}\nTotal prize: ${userPrize} $\nPossible prize on current attempt: ${maxPrize} $\nEnter a number of pocket`);
+
+
+            if (userBet === random && counter === 2) {
+                console.log('counter ' + counter);
+                userPrize += maxPrize;
+                let continueGame = confirm(`Congratulation, you won!   Your prize is: 
+            ${userPrize} $. Do you want to continue?`);
+                if (!continueGame) {
+                    alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
+                    let playAgain = confirm('Do you want to play again?');
+                    if (playAgain) {
+                        initialRange = 9;
+                        totlaMaxPrize = 100;
+                        userPrize = 0;
+                        counter = 3;
+                        continue;
+                    } else {
+                        break;
+                    }
+                } else if (continueGame) {
+                    initialRange += 4;
+                    totlaMaxPrize *= 2;
+                    maxPrize = totlaMaxPrize;
+                    counter = 3;
+                    random = Math.floor(Math.random() * initialRange);
+                    continue;
+                }
+            } else if (userBet === random && counter === 1) {
+                console.log('counter ' + counter);
+                userPrize += maxPrize;
+                continueGame = (`Congratulation, you won!   Your prize is: ${userPrize} $. Do you want to continue?`);
+                if (!continueGame) {
+                    alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
+                    playAgain = confirm('Do you want to play again?')
+                    if (playAgain) {
+                        initialRange = 9;
+                        totlaMaxPrize = 100;
+                        userPrize = 0;
+                        counter = 3;
+                        random = Math.floor(Math.random() * initialRange);
+                        continue;
+                    } else {
+                        break;
+                    }
+
+                } else if (continueGame) {
+                    initialRange += 4;
+                    totlaMaxPrize *= 2;
+                    maxPrize = totlaMaxPrize
+                    counter = 3;
+                    continue;
+                }
+            } else if (userBet === random && counter === 0) {
+                console.log('counter ' + counter);
+                userPrize += maxPrize;
+                continueGame = (`Congratulation, you won!   Your prize is: ${userPrize} $. Do you want to continue?`);
+                if (!continueGame) {
+                    alert(`Thank you for your participation. Your prize is: ${userPrize} $`)
+                    playAgain = confirm('Do you want to play again?')
+                    if (playAgain) {
+                        initialRange = 9;
+                        totlaMaxPrize = 100;
+                        maxPrize = totlaMaxPrize;
+                        userPrize = 0;
+                        counter = 3;
+                        continue;
+                    } else {
+                        break;
+                    }
+
+                } else if (continueGame) {
+                    initialRange += 4;
+                    totlaMaxPrize *= 2;
+                    maxPrize = totlaMaxPrize;
+                    counter = 3;
+                    continue;
+                }
+            } else if (!userBet || userBet !== random) {
+                maxPrize /= 2;
+                continue;
+            } else {
+                alert(`Thank you for your participation. Your prize is: ${userPrize} $`);
+                playAgain = confirm('Do you want to play again?');
+                if (!playAgain) {
+                    break;
+                } else {
+                    initialRange = 9;
+                    userPrize = 0;
+                    totlaMaxPrize = 100;
+                    counter = 3;
+                    continue;
+                }
+            }
+        
     }
 }
