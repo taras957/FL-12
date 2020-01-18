e8eb51b3689cb1a7b87b747b7079672e87e67d2e
Taras <kornyaktaras@gmail.com>
2020-01-18 11:20:28 +0200

    eslint error fixed

---
+++ FL12_HW10/homework/js/fighters_game.js
@@ -0,0 +1,111 @@
+const Fighter = function(options) {
+  let name = options.name;
+  let damage = options.damage;
+  let hp = options.hp;
+  let strength = options.strength;
+  let agility = options.agility;
+  let totalWin = 0;
+  let totalLosses = 0;
+  let currentHealth = hp;
+  let maxRangeofSuccess = 100;
+
+  class Fighter {
+    getName() {
+      return name;
+    }
+    getDamage() {
+      return damage;
+    }
+    getStrength() {
+      return strength;
+    }
+    getAgility() {
+      return agility;
+    }
+    getHealth() {
+      return currentHealth;
+    }
+    logCombatHistory() {
+      console.log(`Name: ${name}, Wins: ${totalWin}, Losses: ${totalLosses}`);
+    }
+    addWin() {
+      return totalWin++;
+    }
+    addLoss() {
+      return totalLosses++;
+    }
+    heal(healpPoint) {
+      let addHp = currentHealth + healpPoint;
+      if (addHp > hp) {
+        return hp;
+      }
+      currentHealth = addHp;
+      return currentHealth;
+    }
+    dealDamage(damagePoint) {
+      let delHp = currentHealth - damagePoint;
+      if (delHp <= 0) {
+         currentHealth = 0;
+        return currentHealth;
+      }
+      currentHealth = delHp;
+      return currentHealth;
+    }
+    attack(obj) {
+      let procentOfSuccess = obj.getAgility() + obj.getStrength();
+      let randomProcent = Math.floor(Math.random() * maxRangeofSuccess - 1) + 1;
+      if (procentOfSuccess > randomProcent) {
+        obj.dealDamage(damage);
+        console.log(`${name} makes ${damage} to ${obj.getName()}`);
+      } else {
+        console.log(`${name} attack missed`);
+      }
+    }
+  }
+
+  return new Fighter();
+};
+
+const myFighter = new Fighter({
+  name: 'Maximus',
+  damage: 55,
+  hp: 50,
+  strength: 30,
+  agility: 25
+});
+
+const myFighter2 = new Fighter({
+  name: 'Gordeus',
+  damage: 85,
+  hp: 50 ,
+  strength: 70,
+  agility: 15
+});
+function buttle(fighter1, fighter2) {
+  if (!fighter1.getHealth()) {
+    console.log(`${fighter1.getName()} is dead and can't fight`);
+    return;
+  } else if (!fighter2.getHealth()) {
+    console.log(`${fighter2.getName()} is dead and can't fight`);
+    return;
+  }
+
+  while (fighter1.getHealth() > 0 && fighter2.getHealth() > 0) {
+    fighter1.attack(fighter2);
+    fighter2.attack(fighter1);
+  }
+  if (fighter1.getHealth()) {
+    fighter1.addWin();
+    fighter2.addLoss();
+    console.log(`${fighter1.getName()} has won`);
+  } else {
+          fighter2.addWin();
+          fighter1.addLoss();
+    console.log(`${fighter2.getName()} has won`);
+  }
+}
+buttle(myFighter, myFighter2);
+
+
+console.log(myFighter.logCombatHistory());
+
